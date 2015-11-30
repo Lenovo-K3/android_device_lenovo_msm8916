1a22d2e3a99b2ccad140e7b07685cf5b972a7e1e
Silentlys <1561707299@qq.com>
2015-11-30 10:15:47 +0800

    ..

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,9 +125,6 @@ TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 # Keymaster
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
 
-# Keymaster
-TARGET_KEYMASTER_WAIT_FOR_QSEE := true
-
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
